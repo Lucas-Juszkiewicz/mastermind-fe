64ce1084c21c5c2a8779837499917289d0e6a4d9
Lucas Juszkiewicz <l.m.juszkiewicz@gmail.com>
2024-06-18 21:46:22 +0200

    MUI menu pops out

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,24 +1,32 @@
-import { Link, NavLink } from "react-router-dom";
+import { Link, NavLink, useNavigate } from "react-router-dom";
 import Logo from "../assets/logo.png";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import { Menu } from "./Menu";
+import { useState } from "react";
 
 export const Header = () => {
+  const navigate = useNavigate();
+  const handleLogin = () => {
+    navigate("/login");
+  };
+
   return (
-    <header>
-      <Link to="/" className="logo">
-        <img src={Logo} alt="Routemate Logo" />
-        <span>Routemate</span>
-      </Link>
-      <nav className="navigation">
-        <NavLink to="/" className="link" end>
-          Home
-        </NavLink>
-        <NavLink to="/products" className="link">
-          Products
-        </NavLink>
-        <NavLink to="/contact" className="link">
-          Contact
-        </NavLink>
-      </nav>
-    </header>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <Menu />
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            News
+          </Typography>
+          <Button color="inherit" onClick={handleLogin}>
+            Login
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
 };
